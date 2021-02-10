65fa21fdb1b70f898332aad46b41f71e585a8e20
Wei-Kai Fang <weikaaai@gmail.com>
2021-02-10 21:00:37 +0800

    main updated

--- main.py
+++ main.py
@@ -23,17 +23,30 @@ for f in glob.glob('./images/*.png'):
     test_ans = np.insert(test_ans, r, np.array([temp_ans]), axis=0)
 
 # activation function
+
+
 def sigmoid(x, d=False):
     if d:
-        return sigmoid(x) * (1 - sigmoid(x))
-    return 1 / (1 + np.exp(1) ** (-x))
+        return x * (1 - x)
+    return 1 / (1 + np.exp(-x))
+
 
 # configs
-iteration = 100000
+iteration = 10
+learning_rate = 0.01
 
 # weights init
 weights_0 = 2 * np.random.random((2500, 2500)) - 1
 weights_1 = 2 * np.random.random((2500, 10)) - 1
 
+# training process
 for i in range(iteration):
-    pass
+    hidden_layer_1 = np.dot(test_img, weights_0) + 1
+    hidden_layer_2 = sigmoid(hidden_layer_1)
+    output_1 = np.dot(hidden_layer_2, weights_1) + 1
+    output_2 = sigmoid(output_1)
+    error = (test_ans - output_2) ** 2
+    print("average error:", np.mean(np.sum(error, axis=0)))
+    delta = 2 * (test_ans - output_2) * sigmoid(output_1, True)
+    delta = np.dot(delta.T, hidden_layer_2).T
+    weights_1 -= delta
